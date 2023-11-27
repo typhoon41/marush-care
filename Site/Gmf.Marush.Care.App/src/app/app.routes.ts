8db7b327fc51ac34716e13df38b794a4fe73388d
typhoon41 <nizzotyphoon41@gmail.com>
2023-11-27 23:31:50 +0100

    Fixes AB#15

--- Site/Gmf.Marush.Care.App/src/app/app.routes.ts
+++ Site/Gmf.Marush.Care.App/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @stylistic/max-len */
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./features/home/home-page.component').then(mod => mod.HomePageComponent) },
+    { path: 'home', loadComponent: () => import('./features/home/home-page.component').then(mod => mod.HomePageComponent) }
+];
