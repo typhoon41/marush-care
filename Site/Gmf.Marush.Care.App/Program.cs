d15273bacbdc4ae4edd76e2f8faf781c36c25286
typhoon41 <nizzotyphoon41@gmail.com>
2023-11-29 00:52:27 +0100

    An attempt to fix angular build.

--- Site/Gmf.Marush.Care.App/Program.cs
+++ Site/Gmf.Marush.Care.App/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseWebRoot("dist");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -8,9 +9,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  _ = app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  _ = app.UseHsts();
 }
 
 app.UseHttpsRedirection();
