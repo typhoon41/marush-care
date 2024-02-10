acc2ef399a78ec867026ec0c707eefd11c3cfe52
typhoon41 <nizzotyphoon41@gmail.com>
2024-02-10 19:23:47 +0100

    Added support for translation of meta and title tags in index.html

--- Site/Gmf.Marush.Care.App/src/app/app.routes.ts
+++ Site/Gmf.Marush.Care.App/src/app/app.routes.ts
@@ -7,20 +7,26 @@ interface TranslatedRoute extends Route {
 export class RoutingDefinition {
     readonly translateRoute = (key: string) => `/${this.routes.filter(route => route.key === key)[0].path}`;
     readonly lazyRoute = (key: string, path: string, loadCallback: () => Promise<unknown>) => ({
-            key,
-            path,
-            loadComponent: loadCallback
-        } as TranslatedRoute);
+        key,
+        path,
+        loadComponent: loadCallback
+    } as TranslatedRoute);
+
+    readonly home = () => $localize`:@@routes.home:početna` as string;
 
     readonly routes: TranslatedRoute[] = [
         {
             key: 'default',
             path: '',
             pathMatch: 'full',
-            redirectTo: $localize`:@@routes.home:početna`
+            redirectTo: this.home()
         },
         this.lazyRoute('home', $localize`:@@routes.home:početna`,
             () => import('./features/home/home-page.component').then(mod => mod.HomePageComponent)),
-        this.lazyRoute('unknown', '**', () => import('./features/home/home-page.component').then(mod => mod.HomePageComponent))
+        {
+            key: 'unknown',
+            path: '**',
+            redirectTo: this.home()
+        }
     ];
 }
--- Site/Gmf.Marush.Care.App/src/app/features/home/home-page.component.ts
+++ Site/Gmf.Marush.Care.App/src/app/features/home/home-page.component.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @stylistic/max-len */
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '@env';
 import { HomeDiscountsComponent } from './discounts/discounts.component';
@@ -8,14 +10,19 @@ import { HomeServicesComponent } from './services/services.component';
 import { HomeSpaceComponent } from './space/space.component';
 
 @Component({
-    selector: 'marush-home-page',
-    standalone: true,
-    imports: [RouterOutlet, HomeHeroComponent, HomeSpaceComponent,
-      HomeServicesComponent, HomeInstagramComponent, HomeDiscountsComponent],
-    templateUrl: './home-page.component.html',
-    styleUrl: './home-page.component.scss'
-  })
-  export class HomePageComponent {
-    readonly mainImageUrl = `${environment.staticContentUrl}images/home/main.png`;
-  }
+  selector: 'marush-home-page',
+  standalone: true,
+  imports: [RouterOutlet, HomeHeroComponent, HomeSpaceComponent,
+    HomeServicesComponent, HomeInstagramComponent, HomeDiscountsComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+  readonly mainImageUrl = `${environment.staticContentUrl}images/home/main.png`;
 
+  constructor(private readonly meta: Meta, private readonly title: Title) {
+    this.meta.updateTag({ name: 'description', content: $localize`:@@routes.home.description:Kozmetički salon Marush: prostor za zdravlje i negu tela. U našem prijatnom okruženju u centru Beograda sprovodimo za Vas kozmetičke procedure nege kože i lica.` });
+    this.meta.updateTag({ name: 'keywords', content: $localize`:@@routes.home.keywords:kozmetički salon,salon lepote,nega lica,nega kože,otklanjanje akni,otklanjanje ožiljaka,tretmani hiperpigmentacije,nega kože akcija,tretmani lica popust,konsultacije,pregled kože,kućna nega lica,Beograd,Vlajkovićeva` });
+    this.title.setTitle($localize`:@@routes.home.title:Marush: Space of Care - početna`);
+  }
+}
