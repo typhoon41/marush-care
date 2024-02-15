006559c7286cc5566516c3001c4cbc0a1caece40
typhoon41 <nizzotyphoon41@gmail.com>
2024-02-15 01:44:31 +0100

    AB#16 => fixing hydration

--- Site/Gmf.Marush.Care.App/src/app/app.config.server.ts
+++ Site/Gmf.Marush.Care.App/src/app/app.config.server.ts
@@ -1,12 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideRouter } from '@angular/router';
 import { appConfig } from './app.config';
-import { RoutingDefinition } from './app.routes';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideRouter(new RoutingDefinition().routes),
     provideServerRendering()
   ]
 };
