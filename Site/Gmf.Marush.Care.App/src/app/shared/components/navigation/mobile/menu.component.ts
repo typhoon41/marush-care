cef9a42a756992e52e615f131fbe27161db9a2c7
typhoon41 <nizzotyphoon41@gmail.com>
2024-02-27 23:58:45 +0100

    Initial version of menu without padding and other spacing.

--- Site/Gmf.Marush.Care.App/src/app/shared/components/navigation/mobile/menu.component.ts
+++ Site/Gmf.Marush.Care.App/src/app/shared/components/navigation/mobile/menu.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from '@env';
 import { BaseRoutingComponent } from '../base-routing.component';
+import { LanguageSelectorComponent } from '../language/selector.component';
 
 @Component({
   selector: 'marush-mobile-menu',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LanguageSelectorComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
