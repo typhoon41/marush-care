ce86bb576da6f58387357ab30012739478797cd7
typhoon41 <nizzotyphoon41@gmail.com>
2024-03-23 23:22:40 +0100

    AB#11 => An attempt to sort out wildcard routing failures. Angular version bump. Trying to setup i18n SSR as per https://github.com/lostium/ssr-i18n-angular17

--- Site/Gmf.Marush.Care.App/server.ts
+++ Site/Gmf.Marush.Care.App/server.ts
@@ -1,22 +1,35 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-magic-numbers */
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
-/* eslint-disable xss/no-mixed-html */
+/* eslint-disable max-lines-per-function */
 /* eslint-disable func-style */
-import { dirname, join, resolve } from 'node:path';
+import { basename, dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { APP_BASE_HREF } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
+import { REQUEST, RESPONSE } from './src/express.tokens';
 import bootstrap from './src/main.server';
 
-// The Express app is exported so that it can be used by serverless Functions.
-// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+  /**
+   * Get the language from the corresponding folder
+   */
+  const lang = basename(serverDistFolder);
+
+  /**
+   * Set the route for static content and APP_BASE_HREF
+   */
+  const langPath = `/${lang}/`;
+
+  /**
+   * Note that the 'browser' folder is located two directories above 'server/{lang}/'
+   */
+  const browserDistFolder = resolve(serverDistFolder, `../../browser/${lang}`);
   const indexHtml = join(serverDistFolder, 'index.server.html');
+
   const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
@@ -25,37 +38,36 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // Server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
+  // Complete the route for static content by concatenating the language.
+  server.get(
+    '*.*',
+    express.static(browserDistFolder, {
+      maxAge: '1y'
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
+    // Discard baseUrl as we will provide it with langPath
+    const { protocol, originalUrl, headers } = req;
     commonEngine
       .render({
         bootstrap,
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
+        // PublicPath does not need to concatenate the language.
+        publicPath: resolve(serverDistFolder, `../../browser/`),
+        providers: [
+          { provide: APP_BASE_HREF, useValue: langPath },
+          { provide: LOCALE_ID, useValue: lang },
+          { provide: RESPONSE, useValue: res },
+          { provide: REQUEST, useValue: req }
+        ]
       })
+      // eslint-disable-next-line xss/no-mixed-html
       .then(html => res.send(html))
       .catch(err => next(err));
   });
 
   return server;
 }
-
-function run(): void {
-  const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-run();
