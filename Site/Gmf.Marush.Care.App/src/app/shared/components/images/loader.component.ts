da6a9a9a55c48f78d716342c509bdfd4ba052c66
typhoon41 <nizzotyphoon41@gmail.com>
2024-05-09 00:05:33 +0200

    Fixed glitches during image loads.

--- Site/Gmf.Marush.Care.App/src/app/shared/components/images/loader.component.ts
+++ Site/Gmf.Marush.Care.App/src/app/shared/components/images/loader.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'marush-image-loader',
@@ -8,13 +8,22 @@ import { Component, Input } from '@angular/core';
     templateUrl: './loader.component.html',
     styleUrl: './loader.component.scss'
 })
-export class ImageLoaderComponent {
+export class ImageLoaderComponent implements OnChanges {
     @Input() url: string = '';
     @Input() description: string = '';
     @Input() givenClass: string = '';
     @Input() selectable: boolean = false;
 
     isLoading: boolean = true;
+
+    // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    ngOnChanges(changes: SimpleChanges) {
+      const urlChanged = changes['url'];
+
+      if (urlChanged) {
+        this.isLoading = true;
+      }
+    }
 
     hideLoader = () => {
       this.isLoading = false;
