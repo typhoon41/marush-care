2fa4e63e9fe17fd3d4d9babb0563d7cb94c15cd5
typhoon41 <nizzotyphoon41@gmail.com>
2024-10-10 01:03:59 +0200

    Started adding EF Core utilities.

---
+++ Packages/Gmf.Net.Core.Common/Persistence/EventsDispatcherStub.cs
@@ -0,0 +1,7 @@
+﻿using Gmf.DDD.Common.Contracts;
+
+namespace Gmf.Net.Core.Common.Persistence;
+public class EventsDispatcherStub : IDispatchEvents<IDomainEvent>
+{
+    public Task Dispatch(IDomainEvent domainEvent) => Task.CompletedTask;
+}
