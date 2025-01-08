d5d430bdf6a6fcf7fcfc88e0482f5cfaaf976381
typhoon41 <nizzotyphoon41@gmail.com>
2025-01-08 02:19:48 +0100

    AB#27 => Merge API and App: Fixing runtime issues.

--- Site/Gmf.Marush.Care.Api/Services/NodeJsRunnerService.cs
+++ Site/Gmf.Marush.Care.Api/Services/NodeJsRunnerService.cs
@@ -6,18 +6,20 @@ namespace Gmf.Marush.Care.Api.Services;
 
 [SuppressMessage("Maintainability", "CA1515:Consider making public types internal",
     Justification = "Has to be public due to reachability through DI")]
-public class NodeJsRunnerService : BackgroundService
+public class NodeJsRunnerService(ILogger<NodeJsRunnerService> logger) : BackgroundService
 {
     private Process? _nodeProcess;
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        logger.LogDebug("Starting Node.js server...");
         StartNodeJsServer();
         return Task.CompletedTask;
     }
 
     public override Task StopAsync(CancellationToken cancellationToken)
     {
+        logger.LogDebug("Stopping Node.js server...");
         StopNodeJsServer();
         return base.StopAsync(cancellationToken);
     }
@@ -31,25 +33,32 @@ public class NodeJsRunnerService : BackgroundService
 
     private void StartNodeJsServer()
     {
-        var processStartInfo = new ProcessStartInfo
+        try
         {
-            FileName = "node",
-            Arguments = "dist/server/server.mjs",
-            WorkingDirectory = AppContext.BaseDirectory,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "node",
+                Arguments = "dist/server/server.mjs",
+                WorkingDirectory = AppContext.BaseDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
 
-        _nodeProcess = new Process { StartInfo = processStartInfo };
+            _nodeProcess = new Process { StartInfo = processStartInfo };
 
-        _nodeProcess.OutputDataReceived += (sender, args) => Console.WriteLine($"Node Output: {args.Data}");
-        _nodeProcess.ErrorDataReceived += (sender, args) => Console.Error.WriteLine($"Node Error: {args.Data}");
+            _nodeProcess.OutputDataReceived += (sender, args) => Console.WriteLine($"Node Output: {args.Data}");
+            _nodeProcess.ErrorDataReceived += (sender, args) => Console.Error.WriteLine($"Node Error: {args.Data}");
 
-        _ = _nodeProcess.Start();
-        _nodeProcess.BeginOutputReadLine();
-        _nodeProcess.BeginErrorReadLine();
+            _ = _nodeProcess.Start();
+            _nodeProcess.BeginOutputReadLine();
+            _nodeProcess.BeginErrorReadLine();
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, "Couldn't start Node.js process: {Message}", exception.Message);
+        }
     }
 
     private void StopNodeJsServer()
@@ -61,9 +70,9 @@ public class NodeJsRunnerService : BackgroundService
                 _nodeProcess.Kill();
                 _ = _nodeProcess.WaitForExit(5000);
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                Console.Error.WriteLine($"Failed to stop Node.js process: {ex.Message}");
+                logger.LogError(exception, "Failed to stop Node.js process: {Message}", exception.Message);
             }
         }
     }
