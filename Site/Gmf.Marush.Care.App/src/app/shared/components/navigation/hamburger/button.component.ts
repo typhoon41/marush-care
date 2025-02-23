bc67df57c8cc8c0a42078ab699ea127ccc164253
Nikola Dragićević <nda@enigmatry.com>
2025-02-23 17:35:28 +0100

    Added aria-labels on all buttons with images/icons but without any text descriptions.

--- Site/Gmf.Marush.Care.App/src/app/shared/components/navigation/hamburger/button.component.ts
+++ Site/Gmf.Marush.Care.App/src/app/shared/components/navigation/hamburger/button.component.ts
@@ -12,19 +12,27 @@ export class HamburgerButtonComponent implements OnInit {
     @Input() collapsed: boolean = false;
     @Output() collapsedChange = new EventEmitter<boolean>();
 
+    private readonly buttonShrinked = $localize`:@@content.menu.open:Otvori meni`;
+    private readonly buttonCollapsed = $localize`:@@content.menu.close:Zatvori meni`;
+    buttonDescription: string = '';
 
     // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
     ngOnInit(): void {
         this.emitEvent();
+        this.setButtonDescription();
     }
 
     readonly toggle = (event?: OptionalKeyboardEvent) => {
         if (isAction(event)) {
             event?.preventDefault();
             this.collapsed = !this.collapsed;
+            this.setButtonDescription();
             this.emitEvent();
         }
     };
 
     private readonly emitEvent = () => this.collapsedChange.emit(this.collapsed);
+    private readonly setButtonDescription = () => {
+        this.buttonDescription = this.collapsed ? this.buttonCollapsed : this.buttonShrinked;
+    };
 }
