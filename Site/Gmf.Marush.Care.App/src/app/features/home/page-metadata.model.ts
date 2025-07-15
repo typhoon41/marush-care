eee7f3617568f19c70918afbdfe2fb49f7d68c68
typhoon41 <nizzotyphoon41@gmail.com>
2025-07-16 01:24:55 +0200

    Removed current discounts.

--- Site/Gmf.Marush.Care.App/src/app/features/home/page-metadata.model.ts
+++ Site/Gmf.Marush.Care.App/src/app/features/home/page-metadata.model.ts
@@ -1,73 +1,15 @@
 /* eslint-disable @stylistic/max-len */
-import { environment } from '@env';
 import { IStructuredData, PageMetadata } from '@shared/components/page/base/page-metadata.model';
 import { ILanguage } from '@shared/models/language.model';
-import { MoneyPipe } from '@shared/pipes/money-pipe';
 
-export const currentDiscounts = [
-    $localize`:@@discounts.1:<h3><strong>9.6. - 16.6.</strong></h3><p>Premium higij. + neinv. mezoter.<br/><br/><s>10.000</s> 8.000</p>`,
-    $localize`:@@discounts.2:<h3><strong>9.6. - 16.6.</strong></h3><p>Hemij. piling + bio. tretman<br/><br/><s>5.500</s> 4.500</p>`
-];
+export const currentDiscounts = [];
 
 export class HomePageMetadata extends PageMetadata {
+    override getSpecificStructuredData: (baseStructuredData: IStructuredData, language: ILanguage) => IStructuredData =
+     (baseStructuredData, _language) => baseStructuredData;
+
     override pathTranslations = () => ({ en: 'home', sr: 'početna', ru: 'начальное' });
     override getTitle = () => $localize`:@@routes.home.title:Marush: Space of Care - početna` as string;
     override getKeywords = () => $localize`:@@routes.home.keywords:kozmetički salon,kozmeticki salon,salon lepote,nega lica,nega kože,otklanjanje akni,otklanjanje ožiljaka,tretmani hiperpigmentacije,tretmani lica popust,konsultacije,pregled kože,kućna nega lica,Beograd,Višegradska` as string;
     override getDescription = () => $localize`:@@routes.home.description:Kozmetički salon Marush: prostor za zdravlje i negu tela. U našem prijatnom okruženju u centru Beograda sprovodimo za Vas kozmetičke procedure nege kože i lica.` as string;
-
-    override getSpecificStructuredData = (baseStructuredData: IStructuredData, _language: ILanguage) => {
-        const specialAnnouncements = {
-            '@type': 'SpecialAnnouncement',
-            '@id': this.marushId('discounts'),
-            announcementLocation: this.marushSalon(),
-            name: $localize`:@@discounts.title:Ponuda ograničenog trajanja`,
-            text: $localize`:@@discounts.contact:Kontaktirajte nas za više detalja!`,
-            url: environment.url,
-            datePosted: '2025-06-10',
-            expires: '2025-06-16',
-            category: $localize`:@@discounts.title:Ponuda ograničenog trajanja`,
-            provider: this.marushSalon(),
-            subjectOf: [
-                this.offer(currentDiscounts[0], '2025-06-16', '10000', '8000'),
-                this.offer(currentDiscounts[1], '2025-06-16', '5500', '4500')
-            ]
-        };
-
-        this.getGraphFrom(baseStructuredData).push(specialAnnouncements);
-        return baseStructuredData;
-    };
-
-    private readonly priceSpecification = (originalPrice: string, discountedPrice: string) =>
-        [
-            {
-                '@type': 'UnitPriceSpecification',
-                price: new MoneyPipe().transform(originalPrice),
-                priceCurrency: 'RSD',
-                valueAddedTaxIncluded: true,
-                eligibleTransactionVolume: {
-                    '@type': 'PriceSpecification',
-                    name: 'Original Price'
-                }
-            },
-            {
-                '@type': 'UnitPriceSpecification',
-                price: new MoneyPipe().transform(discountedPrice),
-                priceCurrency: 'RSD',
-                valueAddedTaxIncluded: true,
-                eligibleTransactionVolume: {
-                    '@type': 'PriceSpecification',
-                    name: 'Discounted Price'
-                }
-            }
-        ];
-
-    private readonly offer = (name: string, priceValidUntil: string, originalPrice: string, price: string) => ({
-            '@type': 'Offer',
-            name,
-            availability: 'https://schema.org/InStock',
-            priceValidUntil,
-            priceCurrency: 'RSD',
-            price: new MoneyPipe().transform(price),
-            priceSpecification: this.priceSpecification(originalPrice, price)
-        });
 }
