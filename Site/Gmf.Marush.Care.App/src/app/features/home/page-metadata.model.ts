5b9dc1ccee34289c5456e9461b7ecfc8f9cf0c0c
typhoon41 <nizzotyphoon41@gmail.com>
2025-07-19 01:59:00 +0200

    Added the latest discounts.

--- Site/Gmf.Marush.Care.App/src/app/features/home/page-metadata.model.ts
+++ Site/Gmf.Marush.Care.App/src/app/features/home/page-metadata.model.ts
@@ -1,15 +1,71 @@
 /* eslint-disable @stylistic/max-len */
+import { environment } from '@env';
 import { IStructuredData, PageMetadata } from '@shared/components/page/base/page-metadata.model';
 import { ILanguage } from '@shared/models/language.model';
+import { MoneyPipe } from '@shared/pipes/money-pipe';
 
-export const currentDiscounts = [];
+export const currentDiscounts = [
+    $localize`:@@discounts.1:<h3><strong>21.7. - 26.7.</strong></h3><p>Neinv. mezoter. + radio talasi + lifting masaža<br/><br/><s>10.500</s> 7.500</p>`
+];
 
 export class HomePageMetadata extends PageMetadata {
-    override getSpecificStructuredData: (baseStructuredData: IStructuredData, language: ILanguage) => IStructuredData =
-     (baseStructuredData, _language) => baseStructuredData;
-
     override pathTranslations = () => ({ en: 'home', sr: 'početna', ru: 'начальное' });
     override getTitle = () => $localize`:@@routes.home.title:Marush: Space of Care - početna` as string;
     override getKeywords = () => $localize`:@@routes.home.keywords:kozmetički salon,kozmeticki salon,salon lepote,nega lica,nega kože,otklanjanje akni,otklanjanje ožiljaka,tretmani hiperpigmentacije,tretmani lica popust,konsultacije,pregled kože,kućna nega lica,Beograd,Višegradska` as string;
     override getDescription = () => $localize`:@@routes.home.description:Kozmetički salon Marush: prostor za zdravlje i negu tela. U našem prijatnom okruženju u centru Beograda sprovodimo za Vas kozmetičke procedure nege kože i lica.` as string;
+
+    override getSpecificStructuredData = (baseStructuredData: IStructuredData, _language: ILanguage) => {
+        const specialAnnouncements = {
+            '@type': 'SpecialAnnouncement',
+            '@id': this.marushId('discounts'),
+            announcementLocation: this.marushSalon(),
+            name: $localize`:@@discounts.title:Ponuda ograničenog trajanja`,
+            text: $localize`:@@discounts.contact:Kontaktirajte nas za više detalja!`,
+            url: environment.url,
+            datePosted: '2025-07-19',
+            expires: '2025-07-26',
+            category: $localize`:@@discounts.title:Ponuda ograničenog trajanja`,
+            provider: this.marushSalon(),
+            subjectOf: [
+                this.offer(currentDiscounts[0], '2025-07-26', '10500', '7500')
+            ]
+        };
+
+        this.getGraphFrom(baseStructuredData).push(specialAnnouncements);
+        return baseStructuredData;
+    };
+
+    private readonly priceSpecification = (originalPrice: string, discountedPrice: string) =>
+        [
+            {
+                '@type': 'UnitPriceSpecification',
+                price: new MoneyPipe().transform(originalPrice),
+                priceCurrency: 'RSD',
+                valueAddedTaxIncluded: true,
+                eligibleTransactionVolume: {
+                    '@type': 'PriceSpecification',
+                    name: 'Original Price'
+                }
+            },
+            {
+                '@type': 'UnitPriceSpecification',
+                price: new MoneyPipe().transform(discountedPrice),
+                priceCurrency: 'RSD',
+                valueAddedTaxIncluded: true,
+                eligibleTransactionVolume: {
+                    '@type': 'PriceSpecification',
+                    name: 'Discounted Price'
+                }
+            }
+        ];
+
+    private readonly offer = (name: string, priceValidUntil: string, originalPrice: string, price: string) => ({
+            '@type': 'Offer',
+            name,
+            availability: 'https://schema.org/InStock',
+            priceValidUntil,
+            priceCurrency: 'RSD',
+            price: new MoneyPipe().transform(price),
+            priceSpecification: this.priceSpecification(originalPrice, price)
+        });
 }
