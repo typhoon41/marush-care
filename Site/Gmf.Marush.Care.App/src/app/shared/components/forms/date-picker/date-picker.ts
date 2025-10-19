870e8d6bbcbf16a3cda4e2f6324428ee4550336c
Nikola Dragićević <nizzotyphoon41@gmail.com>
2025-10-19 21:36:31 +0200

    An attempt to fix datepicker.

--- Site/Gmf.Marush.Care.App/src/app/shared/components/forms/date-picker/date-picker.ts
+++ Site/Gmf.Marush.Care.App/src/app/shared/components/forms/date-picker/date-picker.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, ChangeDetectionStrategy, Component, ElementRef, input, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, input, signal, viewChild } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import Language from '@shared/models/language';
 import AirDatepicker from 'air-datepicker';
@@ -20,39 +20,38 @@ export class DatePicker extends Field {
     // eslint-disable-next-line @angular-eslint/prefer-signals
     private datePicker = signal<AirDatepicker | undefined>(undefined);
 
-    constructor() {
-        super();
-        afterNextRender(() => {
-            const today = new Date();
-            const nextMonth = new Date(new Date().setMonth(today.getMonth() + 1));
-
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            this.datePicker.set(new AirDatepicker(this.date()!.nativeElement, {
-                locale: new Language().predefined().datePickerLocale,
-                autoClose: true,
-                showOtherMonths: false,
-                moveToOtherMonthsOnSelect: false,
-                selectOtherMonths: false,
-                minDate: today,
-                maxDate: nextMonth,
-                onBeforeSelect: ({ date }) => date.getDay() !== 0,
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
-                onSelect: ({ date, formattedDate, datepicker }) => {
-                    this.form().get(this.name())
-                        ?.setValue(formattedDate);
-                },
-                onRenderCell: ({ date, cellType }) => {
-                    if (cellType === 'day' && date.getDay() === 0) {
-                        return {
-                            disabled: true
-                        };
+    protected readonly showDatePicker = () => {
+        if (!this.datePicker()) {
+            {
+                const today = new Date();
+                const nextMonth = new Date(new Date().setMonth(today.getMonth() + 1));
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                this.datePicker.set(new AirDatepicker(this.date()!.nativeElement, {
+                    locale: new Language().predefined().datePickerLocale,
+                    autoClose: true,
+                    showOtherMonths: false,
+                    moveToOtherMonthsOnSelect: false,
+                    selectOtherMonths: false,
+                    minDate: today,
+                    maxDate: nextMonth,
+                    onBeforeSelect: ({ date }) => date.getDay() !== 0,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
+                    onSelect: ({ date, formattedDate, datepicker }) => {
+                        this.form().get(this.name())
+                            ?.setValue(formattedDate);
+                    },
+                    onRenderCell: ({ date, cellType }) => {
+                        if (cellType === 'day' && date.getDay() === 0) {
+                            return {
+                                disabled: true
+                            };
+                        }
+                        return {};
                     }
+                }));
+            }
 
-                    return {};
-                }
-            }));
-        });
-    }
-
-    protected readonly toggleDatePicker = () => this.datePicker()?.show();
+            this.datePicker()?.show();
+        }
+    };
 }
