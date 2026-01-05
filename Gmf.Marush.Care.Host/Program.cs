7a9b37e2218e3f25bfaae20407516f11879b56f3
Nikola Dragićević <nizzotyphoon41@gmail.com>
2026-01-05 02:01:17 +0100

    AB#51 => .Net 10 upgrade: Package upgrade. Solution migrated to slnx.

--- Gmf.Marush.Care.Host/Program.cs
+++ Gmf.Marush.Care.Host/Program.cs
@@ -5,8 +5,11 @@ var sql = builder.AddSqlServer("sql")
                  .WithConnectionStringRedirection(connectionString)
                  .AddDatabase("MarushCare");
 
-builder.AddProject<Projects.Gmf_Marush_Care_Api>("gmf-marush-care-api")
+var api = builder.AddProject<Projects.Gmf_Marush_Care_Api>("gmf-marush-care-api")
     .WithReference(sql);
-//builder.AddProject<Projects.Gmf_Marush_Care_App>("gmf-marush-care-app");
+builder.AddJavaScriptApp("gmf-marush-care-app", "../Site/Gmf.Marush.Care.App", "start")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(port: 4200, env: "PORT");
 
 builder.Build().Run();
